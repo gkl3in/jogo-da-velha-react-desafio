99df288b3ae68953195b213ee4ffd03ddd79ccf4
Gabriel Klein <gabriel.klein@philips.com>
2023-01-09 15:17:57 -0300

    Criada a base do tabuleiro

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './styles.scss';
 
 /*
@@ -16,11 +17,38 @@ import './styles.scss';
 */
 
 function App() {
+  const [gameData, setGameData] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0]);
+  const [turn, setTurn] = useState(1);
+
+  const handleClick = (clickedIndex) => {
+    console.log(clickedIndex);
+
+    if (gameData[clickedIndex] !== 0) {
+      return;
+    }
+
+    setGameData((prev) => {
+      const newGameData = [...prev];
+      newGameData[clickedIndex] = turn;
+      return newGameData;
+    });
+
+    setTurn((prev) => (prev === 1 ? 2 : 1));
+  };
+
   return (
     <>
-      <h3>desafio fernandev</h3>
-      <h1>jogo da velha</h1>
-      <div className='board-game'></div>
+      <div className="board-game">
+        {gameData.map((value, index) => (
+          <span 
+          onClick={() => {
+            handleClick(index);
+          }}
+            >{value === 1 && 'x'}
+            {value === 2 && 'O'}
+            </span>
+        ))}
+      </div>
     </>
   );
 }
